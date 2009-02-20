cbecfc9dcc59713320259be20b74c3422d87ecc6
francois <francois@bacab573-a924-0410-b505-ece3e248c36b>
2009-02-20 15:46:19 +0000

    Removing gems that were previously bundled in the application:  we now install them on the instances where we deploy.
    Make sure the gems are installed locally:
    
    rake gems:install
    
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@15430 bacab573-a924-0410-b505-ece3e248c36b

--- vendor/gems/mechanize-0.8.3/lib/mechanize.rb
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2005 by Michael Neumann (mneumann@ntecs.de) 
-# Copyright (c) 2007 by Aaron Patterson (aaronp@rubyforge.org) 
-#
-# Please see the LICENSE file for licensing.
-
-
-require 'www/mechanize'
