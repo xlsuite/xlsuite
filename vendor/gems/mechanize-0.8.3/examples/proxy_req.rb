762ef7c8a068823fa33065b641604a4c44e8e5bd
francois <francois@bacab573-a924-0410-b505-ece3e248c36b>
2009-02-20 15:46:19 +0000

    Removing gems that were previously bundled in the application:  we now install them on the instances where we deploy.
    Make sure the gems are installed locally:
    
    rake gems:install
    
    
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@15430 bacab573-a924-0410-b505-ece3e248c36b

--- vendor/gems/mechanize-0.8.3/examples/proxy_req.rb
+++
@@ -1,9 +0,0 @@
-$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
-
-require 'rubygems'
-require 'mechanize'
-
-agent = WWW::Mechanize.new
-agent.set_proxy('localhost', '8000')
-page = agent.get(ARGV[0])
-puts page.body
