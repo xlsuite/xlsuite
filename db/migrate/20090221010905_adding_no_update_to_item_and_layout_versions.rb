ad2ae25bb825e69b3c63be3490faf4711d89c349
harman <harman@bacab573-a924-0410-b505-ece3e248c36b>
2009-02-21 01:16:31 +0000

    It's ItemVersion and LayoutVersion not Revision
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@15469 bacab573-a924-0410-b505-ece3e248c36b

--- db/migrate/20090221010905_adding_no_update_to_item_and_layout_versions.rb
+++ db/migrate/20090221010905_adding_no_update_to_item_and_layout_versions.rb
@@ -1,14 +1,14 @@
 class AddingNoUpdateToItemAndLayoutVersions < ActiveRecord::Migration
   def self.up
-    add_column :layout_revisions, :no_update, :boolean, :default => false
-    add_column :item_revisions, :no_update, :boolean, :default => false
+    add_column :layout_versions, :no_update, :boolean, :default => false
+    add_column :item_versions, :no_update, :boolean, :default => false
   end
 
   def self.down
-    remove_column :layout_revisions, :no_update
-    remove_column :item_revisions, :no_update
+    remove_column :layout_versions, :no_update
+    remove_column :item_versions, :no_update
   end
   
-  class ItemRevision < ActiveRecord::Base;end
-  class LayoutRevision < ActiveRecord::Base;end
+  class ItemVersion < ActiveRecord::Base;end
+  class LayoutVersion < ActiveRecord::Base;end
 end
