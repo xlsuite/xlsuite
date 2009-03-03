96c50ceca4d87533e4742d75a7ac0367af75161a
harman <harman@bacab573-a924-0410-b505-ece3e248c36b>
2009-03-03 23:49:17 +0000

    Implemented #embed_code action and defined its routing BugzID:301116
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@15584 bacab573-a924-0410-b505-ece3e248c36b

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,8 @@ ActionController::Routing::Routes.draw do |map|
     :collection => {:destroy_collection => :delete}
   map.resources :public_groups, :path_prefix => "/admin/public", :as => "groups", :controller => "public/groups", 
     :member => {:join => :post, :leave => :post}
-  map.resources :public_listings, :path_prefix => "/admin/public", :as => "listings", :controller => "public/listings"
+  map.resources :public_listings, :path_prefix => "/admin/public", :as => "listings", :controller => "public/listings",
+    :member => {:embed_code => :get}
   map.resources :public_blogs, :path_prefix => "/admin/public", :as => "blogs", :controller => "public/blogs",
     :collection => {:validate_label => :post}
   map.resources :public_blog_posts, :path_prefix => "/admin/public", :as => "blog_posts", :controller => "public/blog_posts"
