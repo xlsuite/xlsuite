767e0fb0598c45b4902336acde1e30f655a58986
harman <harman@bacab573-a924-0410-b505-ece3e248c36b>
2009-03-03 00:12:57 +0000

    Removing extjs source BugzID:301119
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@15566 bacab573-a924-0410-b505-ece3e248c36b

--- public/javascripts/extjs-src/examples/panel/panels.js
+++
@@ -1,9 +0,0 @@
-Ext.onReady(function(){
-    var p = new Ext.Panel({
-        title: 'My Panel',
-        collapsible:true,
-        renderTo: 'container',
-        width:400,
-        html: Ext.example.bogusMarkup
-    });
-});
--- public/javascripts/extjs-src/examples/portal/Portlet.js
+++
@@ -1,8 +0,0 @@
-Ext.ux.Portlet = Ext.extend(Ext.Panel, {
-    anchor: '100%',
-    frame:true,
-    collapsible:true,
-    draggable:true,
-    cls:'x-portlet'
-});
-Ext.reg('portlet', Ext.ux.Portlet);
