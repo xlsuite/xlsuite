196f58bd37a6dbe9f365fa0938e618ba4951c38d
harman <harman@bacab573-a924-0410-b505-ece3e248c36b>
2009-03-03 00:12:57 +0000

    Removing extjs source BugzID:301119
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@15566 bacab573-a924-0410-b505-ece3e248c36b

--- public/javascripts/extjs-src/examples/desktop/js/Module.js
+++
@@ -1,9 +0,0 @@
-Ext.app.Module = function(config){
-    Ext.apply(this, config);
-    Ext.app.Module.superclass.constructor.call(this);
-    this.init();
-}
-
-Ext.extend(Ext.app.Module, Ext.util.Observable, {
-    init : Ext.emptyFn
-});
