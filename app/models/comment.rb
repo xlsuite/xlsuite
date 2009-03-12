f0d3c921f94e8e74201e0debecc40c89cb2d5aa2
spencer <spencer@bacab573-a924-0410-b505-ece3e248c36b>
2009-03-12 19:09:50 +0000

    Case insensitive regexp when looking for blacklist_words in contact requests and comments
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@15668 bacab573-a924-0410-b505-ece3e248c36b

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -312,7 +312,7 @@ class Comment < ActiveRecord::Base
     blacklist_words = self.domain.get_config("blacklist_words")
     blacklist_words_array = blacklist_words.split(',').map(&:strip).reject(&:blank?)
     return false if blacklist_words_array.join("").blank?
-    blacklist_regex = Regexp.new("(#{blacklist_words_array.join('|')})")
+    blacklist_regex = Regexp.new("(#{blacklist_words_array.join('|')})", true)
     %w(name url email body).each do |column|
       return true if self.send(column) =~ blacklist_regex
     end
