98f86641b69231c8d06ad2dd54086f703011785f
harman <harman@bacab573-a924-0410-b505-ece3e248c36b>
2009-03-17 00:18:07 +0000

    Defining #humanize method
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@15696 bacab573-a924-0410-b505-ece3e248c36b

--- lib/xl_suite/liquid/standard_filters_extra.rb
+++ lib/xl_suite/liquid/standard_filters_extra.rb
@@ -309,6 +309,10 @@ module XlSuite
         return nil unless input.respond_to?(:strip)
         input.strip
       end
+      
+      def humanize(input)
+        input.humanize
+      end
     end
   end
 end
