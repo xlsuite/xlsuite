b981982f292ec3a2557924bff56d7bf866240e64
spencer <spencer@bacab573-a924-0410-b505-ece3e248c36b>
2009-03-23 23:42:43 +0000

    ContactRequestDrop#created_at
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@15760 bacab573-a924-0410-b505-ece3e248c36b

--- app/drops/contact_request_drop.rb
+++ app/drops/contact_request_drop.rb
@@ -280,7 +280,7 @@
 # 		     END OF TERMS AND CONDITIONS
 class ContactRequestDrop < Liquid::Drop
   attr_reader :contact_request
-  delegate :name, :email, :phone, :time_to_call, :subject, :body, :referrer_url, :tag_list, 
+  delegate :name, :email, :phone, :time_to_call, :subject, :body, :referrer_url, :tag_list, :created_at,
     :party, :completed_at, :approved_at, :recipients, :affiliate, :to => :contact_request
   
   def initialize(contact_request)
