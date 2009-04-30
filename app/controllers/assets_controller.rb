01ca748a5b3a650dd33292112072782abfe704b1
spencer <spencer@bacab573-a924-0410-b505-ece3e248c36b>
2009-04-30 02:30:16 +0000

    Show error messages when upload from image picker fails
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@16024 bacab573-a924-0410-b505-ece3e248c36b

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -526,7 +526,7 @@ class AssetsController < ApplicationController
         else
           flash_failure :now, "Upload failed: #{@asset.errors.full_messages.join(',')}"
         end
-        render :inline => %Q`<%= "{success:#{@created}, file_name: '#{@asset.reload.filename}', asset_id: #{@asset.id}, flash: '#{flash[:notice].to_s}',reload_store: '#{params[:object_type]}_#{@ar_object.blank? ? 0 : @ar_object.id}_#{params[:mode]}_#{params[:classification]}', asset_download_path: '#{@asset.z_src}'}" %>`
+        render :inline => %Q`<%= "{success:#{@created}, file_name: '#{@created ? @asset.reload.filename : @asset.filename}', asset_id: #{@created ? @asset.id : 0}, flash: '#{flash_messages_to_s}',reload_store: '#{params[:object_type]}_#{@ar_object.blank? ? 0 : @ar_object.id}_#{params[:mode]}_#{params[:classification]}', asset_download_path: '#{@asset.z_src}'}" %>`
       end
       format.js do
         if @created
