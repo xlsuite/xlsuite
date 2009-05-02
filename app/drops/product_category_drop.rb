3b04c04e9b5ceb029a81e476d161b964babb6127
harman <harman@bacab573-a924-0410-b505-ece3e248c36b>
2009-05-02 03:07:59 +0000

    Defining #products instead of delegating it to the ProductCategory model
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@16035 bacab573-a924-0410-b505-ece3e248c36b

--- app/drops/product_category_drop.rb
+++ app/drops/product_category_drop.rb
@@ -280,7 +280,7 @@
 # 		     END OF TERMS AND CONDITIONS
 class ProductCategoryDrop < Liquid::Drop
   attr_reader :product_category
-  delegate :id, :name, :children, :ancestors, :siblings, :parent, :label, :products, :avatar, 
+  delegate :id, :name, :children, :ancestors, :siblings, :parent, :label, :avatar, 
            :to => :product_category
 
   def initialize(product_category)
@@ -289,6 +289,11 @@ class ProductCategoryDrop < Liquid::Drop
 
   alias_method :main_image, :avatar
   alias_method :picture, :avatar
+
+  def products
+    product_ids = ActiveRecord::Base.connection.select_values("SELECT product_id FROM product_categories_products WHERE product_category_id = #{self.product_category.id}")
+    Product.find(:all, :conditions => {:id => product_ids}, :order => "LOWER(name) ASC")
+  end
 
   def description
     product_category.web_copy.blank? ? (product_category.description || "") : product_category.web_copy
