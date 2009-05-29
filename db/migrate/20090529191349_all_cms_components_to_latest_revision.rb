06cd68745b5c310b2e2f74103f510a7c32f971ea
harman <harman@bacab573-a924-0410-b505-ece3e248c36b>
2009-05-29 21:22:00 +0000

    Update to latest migration that bring cms components to latest revision
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@16288 bacab573-a924-0410-b505-ece3e248c36b

--- db/migrate/20090529191349_all_cms_components_to_latest_revision.rb
+++ db/migrate/20090529191349_all_cms_components_to_latest_revision.rb
@@ -3,36 +3,41 @@ class AllCmsComponentsToLatestRevision < ActiveRecord::Migration
     latest_version = nil
     current_version = nil
   
-    Page.all.each do |page|
-      latest_version = page.versions.latest.version
-      current_version = page.version
-      if latest_version > current_version
-        page.revert_to!(latest_version)
-      elsif latest_version < current_version
-        page.body = page.body + " "
-        page.save
-      end
-    end
+    account_ids = Account.all(:select => "id").map(&:id)
+    account_ids = account_ids.reject(&:blank?)
     
-    Snippet.all.each do |snippet|
-      latest_version = snippet.versions.latest.version
-      current_version = snippet.version
-      if latest_version > current_version
-        snippet.revert_to!(latest_version)
-      elsif latest_version < current_version
-        snippet.body = snippet.body + " "
-        snippet.save
+    account_ids.each do |account_id|
+      Page.all(:conditions => {:account_id => account_id}).each do |page|
+        latest_version = page.versions.latest.version
+        current_version = page.version
+        if latest_version > current_version
+          page.revert_to!(latest_version)
+        elsif latest_version < current_version
+          page.body = page.body + " "
+          page.save
+        end
       end
-    end
-    
-    Layout.all.each do |layout|
-      latest_version = layout.versions.latest.version
-      current_version = layout.version
-      if latest_version > current_version
-        layout.revert_to!(latest_version)
-      elsif latest_version < current_version
-        layout.body = layout.body + " "
-        layout.save
+      
+      Snippet.all(:conditions => {:account_id => account_id}).each do |snippet|
+        latest_version = snippet.versions.latest.version
+        current_version = snippet.version
+        if latest_version > current_version
+          snippet.revert_to!(latest_version)
+        elsif latest_version < current_version
+          snippet.body = snippet.body + " "
+          snippet.save
+        end
+      end
+      
+      Layout.all(:conditions => {:account_id => account_id}).each do |layout|
+        latest_version = layout.versions.latest.version
+        current_version = layout.version
+        if latest_version > current_version
+          layout.revert_to!(latest_version)
+        elsif latest_version < current_version
+          layout.body = layout.body + " "
+          layout.save
+        end
       end
     end
   end
