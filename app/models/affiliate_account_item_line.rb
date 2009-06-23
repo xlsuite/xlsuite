a26c81d89a6e3038d20e1ec7f8a8e3c81e68e124
harman <harman@bacab573-a924-0410-b505-ece3e248c36b>
2009-06-23 22:41:04 +0000

    Initial implementation of the new AffiliateAccountItemLine model
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@16582 bacab573-a924-0410-b505-ece3e248c36b

---
+++ app/models/affiliate_account_item_line.rb
@@ -0,0 +1,10 @@
+class AffiliateAccountItemLine < ActiveRecord::Base
+  belongs_to :affiliate_account_item
+  belongs_to :target, :polymorphic => true
+  
+  acts_as_money :price
+  acts_as_money :commission
+  
+  validates_presence_of :affiliate_account_item_id, :target_type, :target_id
+  validates_uniqueness_of :target_id, :scope => [:affiliate_account_item_id, :target_type]
+end
