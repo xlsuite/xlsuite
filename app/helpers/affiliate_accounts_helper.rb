36eed2aa3659091d20a63be8d9fbe4e74f981aad
harman <harman@bacab573-a924-0410-b505-ece3e248c36b>
2009-07-02 23:09:51 +0000

    Fixing blank image in AffiliateAccountsHelper
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@16645 bacab573-a924-0410-b505-ece3e248c36b

--- app/helpers/affiliate_accounts_helper.rb
+++ app/helpers/affiliate_accounts_helper.rb
@@ -3,11 +3,11 @@ module AffiliateAccountsHelper
     %Q`
       <ul id='group-by-referred-items-menu'>
         <li>
-		      <img src="images/s.gif" class="group-by-item">
+		      <img src="/javascripts/extjs/resources/images/default/s.gif" class="group-by-item">
 		      <a id="group-by-item" href="#">By Item</a>
         </li>
         <li>
-		      <img src="images/s.gif" class="group-by-domain">
+		      <img src="/javascripts/extjs/resources/images/default/s.gif" class="group-by-domain">
 		      <a id="group-by-domain" href="#">By Domain</a>
         </li>
       </ul>
