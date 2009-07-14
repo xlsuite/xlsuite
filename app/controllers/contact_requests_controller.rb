906a430ffee09076804eb708cc9dc8d987e16e40
spencer <spencer@bacab573-a924-0410-b505-ece3e248c36b>
2009-07-14 23:22:04 +0000

    Move flash liquid assignment
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@16752 bacab573-a924-0410-b505-ece3e248c36b

--- app/controllers/contact_requests_controller.rb
+++ app/controllers/contact_requests_controller.rb
@@ -431,6 +431,10 @@ class ContactRequestsController < ApplicationController
         @error_messages << @contact_request.errors.full_messages
       end
     end
+    
+    flash[:liquid] ||= {}
+    flash[:liquid][:params] = params
+    
     if !@error_messages.blank?
       @error_messages = @error_messages.flatten
       flash_failure @error_messages
@@ -441,9 +445,6 @@ class ContactRequestsController < ApplicationController
       return redirect_to(params[:return_to]) unless params[:return_to].blank?
       return render(:text => "<html><head><title></title></head><body><h1>Thanks</h1><p>Thank you for your submission</p></body></html>")
     end
-    
-    flash[:liquid] ||= {}
-    flash[:liquid][:params] = params
     
     return redirect_to(:back) if request.env["HTTP_REFERER"]
     render(:missing)
