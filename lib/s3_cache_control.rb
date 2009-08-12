0960fe9e828b74a312aab4ef7e0ac313b95cbd67
harman <harman@bacab573-a924-0410-b505-ece3e248c36b>
2009-08-12 02:11:29 +0000

    Adding #set_cache_control in lib/s3_cache_control.rb under the AttachmentFu extension
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@16934 bacab573-a924-0410-b505-ece3e248c36b

--- lib/s3_cache_control.rb
+++ lib/s3_cache_control.rb
@@ -17,16 +17,27 @@ module AWS
 end
 
 require 'technoweenie/attachment_fu/backends/s3_backend'
-  module Technoweenie
-    module AttachmentFu
-      module Backends
-        module S3Backend
+module Technoweenie
+  module AttachmentFu
+    module Backends
+      module S3Backend
         def authenticated_s3_url(*args)
           options = args.extract_options!
           options[:expires] = Time.now.advance(:minutes => 5).end_of_day.to_i if options[:expires].blank? and options[:expires_on].blank?
           thumbnail = args.shift
           S3Object.url_for(full_filename(thumbnail), bucket_name, options)
         end
+
+        def set_cache_control
+          begin
+            s3_object = AWS::S3::S3Object.find(full_filename, bucket_name)
+            s3_object.cache_control = 'max-age=315360000'
+            s3_object.save({:access => :public_read})
+          rescue Exception => e
+            logger.error("Unable to update asset with key " +
+              "#{self.full_filename}: #{e}")
+          end
+        end  
       end
     end
   end
