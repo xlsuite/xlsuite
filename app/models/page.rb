e50ec953b452e4d83f82e2911361dc067b65177d
harman <harman@bacab573-a924-0410-b505-ece3e248c36b>
2009-08-25 21:53:00 +0000

    Page after_save callback to refresh cached pages based on page fullslug
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@17043 bacab573-a924-0410-b505-ece3e248c36b

--- app/models/page.rb
+++ app/models/page.rb
@@ -308,6 +308,8 @@ class Page < Item
 
   validate :title_template_syntax
   before_save :cache_title_template
+  
+  after_save :refresh_cached_pages_with_fullslug
 
   serialize :requirements
 
@@ -572,5 +574,9 @@ class Page < Item
 
   def cache_title_template
     self.cached_parsed_title = Marshal.dump(self.parse_title_template)
+  end
+  
+  def refresh_cached_pages_with_fullslug
+    CachedPage.force_refresh_on_account_fullslug!(self.account, self.fullslug)
   end
 end
