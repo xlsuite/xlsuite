48f8c19149e7ce1b4ff34d3171525ca2be962582
harman <harman@bacab573-a924-0410-b505-ece3e248c36b>
2009-09-03 01:10:03 +0000

    Implemented before_validation :set_label_if_blank for ActionHandler belongs_to :account
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@17117 bacab573-a924-0410-b505-ece3e248c36b

--- app/models/action_handler.rb
+++ app/models/action_handler.rb
@@ -1,4 +1,48 @@
 class ActionHandler < ActiveRecord::Base
+  belongs_to :account
+  
   validates_presence_of :label, :name, :account_id
   validates_uniqueness_of :label, :scope => :account_id
+  
+  before_validation :set_label_if_blank
+  
+  protected
+  def set_label_if_blank
+    return unless self.label.blank?
+    self.set_label
+  end
+
+  def set_label
+    t_label = self.name.to_s.dup
+    t_label.gsub!(/[^(\d\w\s\-_)]/, "")
+    t_label.gsub!(/\s+/, " ")
+    t_label.downcase!
+    t_label.gsub!(/\s/, "-")
+    c_label, t_object = nil, nil
+    unless t_label.blank?
+      count, counter = 0, 0
+      c_label = t_label
+      loop do
+        count = self.class.count(:conditions => {:label => c_label, :account_id => self.account.id})
+        counter += 1
+        if count > 0
+          t_object = self.class.find(:all, :select => "id", :conditions => {:label => c_label, :account_id => self.account.id}).map(&:id)
+          if t_object.size > 1
+            logger.warn("You should not see this message, found the cause in #{self.class.name}#set_label")
+            return
+          end
+          if t_object.first == self.id
+            return
+          else
+            c_label = t_label + counter.to_s
+          end
+        else
+          break
+        end
+      end
+    else
+      c_label = "#{self.class.name.underscore}-#{self.id}" unless self.new_record?
+    end
+    self.label = c_label
+  end  
 end
