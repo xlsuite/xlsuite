667f3f1140a27c7adf19d8b5c686ddd57f3b9805
spencer <spencer@bacab573-a924-0410-b505-ece3e248c36b>
2009-09-15 02:17:59 +0000

    DomainAvailableItem belongs_to :domain
    git-svn-id: https://svn.xlsuite.org/internal/weputuplights.com/trunk@17200 bacab573-a924-0410-b505-ece3e248c36b

--- app/models/domain_available_item.rb
+++ app/models/domain_available_item.rb
@@ -2,4 +2,5 @@ class DomainAvailableItem < ActiveRecord::Base
   validates_presence_of :domain_id, :account_id, :item_type, :item_id
   validates_uniqueness_of :domain_id, :scope => [:item_id, :item_type]
   belongs_to :item, :polymorphic => true
+  belongs_to :domain
 end
